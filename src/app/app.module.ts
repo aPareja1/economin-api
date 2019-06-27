88a044f283942978b1dcc5af68909f1a619ee457
Alejandro Pareja <alejopar2012@gmail.com>
2019-06-27 19:32:06 -0400

    Se agregan los siguientes cambios: - Se genera la carpeta components: 	-Detalle-moneda 	-Lista-indicadores -Se genera la carpeta services: 	-Data-service 	-Moneda-service -Graficos con Chart.js -Conexión con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaIndicadoresComponent } from './components/lista-indicadores/lista-indicadores.component';
+import { DetalleMonedaComponent } from './components/detalle-moneda/detalle-moneda.component';
+import { MonedasService } from './services/monedas.service'
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaIndicadoresComponent,
+    DetalleMonedaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    HttpClientModule,
+      ],
+  providers: [MonedasService,DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
